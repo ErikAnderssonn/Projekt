1030f5dc70539813083ff368f31541247670bd0c
Fredrik Björnek <fredrikbjornek@Fredriks-Air.lan>
2018-05-23 13:26:22 +0200

    oklar

--- nyttrecept.php
+++ nyttrecept.php
@@ -7,3 +7,25 @@
 <script src="Index.js"></script>
 
 <h1 class= "heading" > Studentmat </h1>
+
+<ul>
+
+<div class="navigation">
+
+  Pris/matlåda <br>
+  <input type="text" name="Name" id="Name">
+  <br>
+  Rubrik<br>
+  <input type="text" id="comment" name="comment" form="usrform">
+  <br>
+  Recept
+  <br>
+  <textarea name="message" rows="20" cols="18">Ingredienser
+-
+-
+Utförande
+-
+-</textarea>
+  <br>
+  <input type="submit" class='blimedlem' value="Skicka"/>
+</ul>
