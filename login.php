223c2621c937766e1474c81bf3a8a013494bdccc
Max Mohlin <maxmohlin@Maxs-MBP.lan>
2018-05-26 17:03:58 +0200

    login.php etc

--- login.php
+++ login.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <meta charset="utf-8">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html>
+<meta charset="utf-8">
+   <link href="loginCSS.css" rel="stylesheet">
+<div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="Email"/>
+      <input type="password" placeholder="Lösenord"/>
+      <button>Logga in</button>
+      <p class="message">Inte medlem? <a href="#">Skapa konto</a></p>
+    </form>
+  </div>
+</div>
